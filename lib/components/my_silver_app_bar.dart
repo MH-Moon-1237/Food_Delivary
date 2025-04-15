4014d78034a1464008beee85a31445957d466fdf
MH-Moon-1237 <mhmoon1237@gmail.com>
2025-04-16 03:24:29 +0600

    Final Code

---
+++ lib/components/my_silver_app_bar.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:foodie/pages/cart_page.dart';
+
+class MySliverAppBar extends StatelessWidget {
+  final Widget child;
+  final Widget title;
+
+  const MySliverAppBar({
+    super.key,
+    required this.child,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 480,
+      collapsedHeight: 120,
+      floating: false,
+      pinned: true,
+      actions: [
+        IconButton(
+          onPressed: () {
+            // go to the page
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const CartPage(),
+              ),
+            );
+          },
+          icon: const Icon(Icons.shopping_cart_outlined),
+        ),
+      ],
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+      title: const Text("Sunset Diner"),
+      flexibleSpace: FlexibleSpaceBar(
+        background: Padding(
+          padding: const EdgeInsets.only(bottom: 50.0),
+          child: child,
+        ),
+        title: title,
+        centerTitle: true,
+        titlePadding: const EdgeInsets.only(left: 0, right: 0, top: 0),
+        expandedTitleScale: 1,
+      ),
+    );
+  }
+}
